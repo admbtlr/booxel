027423c1c4a289b85d05de12d9bffbc6ad175cd5
adamvert <ab@adam-butler.com>
2013-05-10 12:30:14 +0200

    tried not unlinking mobi files

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,7 +3,7 @@ class Job
   def send_mobi_to_kindle(order)
     mobi_file = order.get_watermarked_mobi_file
     OrderFulfiller.send_to_kindle(order, mobi_file).deliver()
-    File.unlink(mobi_file.path)
+    # File.unlink(mobi_file.path)
   end
   handle_asynchronously :send_mobi_to_kindle
 
