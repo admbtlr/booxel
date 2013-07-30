c26d059cd81e63fa226971052971a31841bb3ca1
adamvert <ab@adam-butler.com>
2013-07-30 21:25:20 +0200

    fixes screwy migration problem at heroku end

--- db/migrate/20130510153906_remove_sent_to_kindle_at_and_date_from_order.rb
+++
@@ -1,6 +0,0 @@
-class RemoveSentToKindleAtAndDateFromOrder < ActiveRecord::Migration
-  def change
-    remove_column :orders, :sent_to_kindle_at, :string
-    remove_column :orders, :date, :string
-  end
-end
